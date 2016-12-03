3a61fb189cc3584c6c6ff0bece53486dbbc3724f
ParthaB <pbp2005@gmail.com>
2016-12-02 23:59:25 -0500

    Commit

---
+++ py1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+#This program prints two lines.
+
+def hello(str):
+	print str
+	return;
+
+hello(str = "Q: Why do Java Programmers have to wear glasses?")
+hello(str = "A: Because they don't C# :')) Hello World!")
